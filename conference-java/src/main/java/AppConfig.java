38f2d0f952487c41c33a722e83c7a4541ebf87bd
Andreas Hailu <andreashailu@gmail.com>
2020-01-27 06:09:00 -0500

    Began XML configuration style

--- conference-java/src/main/java/AppConfig.java
+++ conference-java/src/main/java/AppConfig.java
@@ -1,18 +1,17 @@
-import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
-import com.pluralsight.repository.SpeakerRepository;
-import com.pluralsight.service.SpeakerService;
-import com.pluralsight.service.SpeakerServiceImpl;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan({"com.pluralsight"})
 public class AppConfig {
 
+    /*
     @Bean(name = "speakerService")
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService() {
-        SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
+//        SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
 //        service.setRepository(getSpeakerRepository());
-
+        SpeakerServiceImpl service = new SpeakerServiceImpl();
         return service;
     }
 
@@ -20,4 +19,5 @@ public class AppConfig {
     public SpeakerRepository getSpeakerRepository() {
         return new HibernateSpeakerRepositoryImpl();
     }
+     */
 }
--- conference-java/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ conference-java/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -2,17 +2,27 @@ package com.pluralsight.service;
 
 import com.pluralsight.model.Speaker;
 import com.pluralsight.repository.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("speakerService")
 public class SpeakerServiceImpl implements SpeakerService {
     private SpeakerRepository repository;
 
+    public SpeakerServiceImpl() {
+        System.out.println("SpeakerServiceImpl no args constructor");
+    }
+
+    @Autowired
     public SpeakerServiceImpl(SpeakerRepository repository) {
+        System.out.println("SpeakerServiceImpl repository constructor");
         this.repository = repository;
     }
 
     public void setRepository(SpeakerRepository repository) {
+        System.out.println("SpeakerServiceImpl repository setter");
         this.repository = repository;
     }
 
