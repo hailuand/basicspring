406b07942603e80b6c432aa2763c0ed00582a0ec
Andreas Hailu <andreashailu@gmail.com>
2020-01-28 06:12:43 -0500

    FactoryBean example

--- conference-java/src/main/java/AppConfig.java
+++ conference-java/src/main/java/AppConfig.java
@@ -1,9 +1,26 @@
+import com.pluralsight.util.CalendarFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Calendar;
 
 @Configuration
 @ComponentScan({"com.pluralsight"})
 public class AppConfig {
+
+    @Bean(name = "cal")
+    public CalendarFactory calFactory() {
+        CalendarFactory calendarFactory = new CalendarFactory();
+        calendarFactory.addDays(2);
+
+        return calendarFactory;
+    }
+
+    @Bean
+    public Calendar cal() {
+        return calFactory().getObject();
+    }
 
     /*
     @Bean(name = "speakerService")
--- conference-java/src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ conference-java/src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,13 +1,19 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Repository("speakerRepository")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    @Autowired
+    Calendar cal;
+
     @Override
     public List<Speaker> findAll() {
         List<Speaker> speakers = new ArrayList<>();
@@ -16,6 +22,8 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
         speaker.setFirstName("Andreas");
         speaker.setLastName("Hailu");
         speakers.add(speaker);
+
+        System.out.println("cal: " + cal.getTime());
 
         return speakers;
     }
