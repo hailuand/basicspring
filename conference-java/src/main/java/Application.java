adacd57a4d0b9428ded510a638456487fc54348b
Andreas Hailu <andreashailu@gmail.com>
2020-01-28 06:20:28 -0500

    Spring expression language

--- conference-java/src/main/java/Application.java
+++ conference-java/src/main/java/Application.java
@@ -8,11 +8,12 @@ public class Application {
 
 //        SpeakerService speakerService = new SpeakerServiceImpl();
         SpeakerService speakerService = appContext.getBean("speakerService", SpeakerService.class);
-        System.out.println(speakerService);
+//        System.out.println(speakerService);
 
         System.out.println(speakerService.findAll().get(0).getFirstName());
+        System.out.println(speakerService.findAll().get(0).getSeedNum());
 
-        SpeakerService speakerService2 = appContext.getBean("speakerService", SpeakerService.class);
-        System.out.println(speakerService2);
+//        SpeakerService speakerService2 = appContext.getBean("speakerService", SpeakerService.class);
+//        System.out.println(speakerService2);
     }
 }
